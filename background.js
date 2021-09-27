57a7cb7183b42b36b16ed74ffd3d935a0d5d7ff7
joyful <blackjoyful@163.com>
2021-09-27 10:26:02 +0800

    页面设计、数据结构设计

--- background.js
+++ background.js
@@ -1,24 +1,41 @@
-var job = "";
-// chrome.proxy.settings.set(setJob())
-
-function setValue(key,value){
-	localStorage.setItem(key,value)
-}
-function getValue(key){
-	return localStorage.getItem(key);
-}
-
-function setJob(str){
-	job = str;
-	chrome.proxy.settings.set({
-		value: {
-			mode: "pac_script",
-			pacScript: {
-				data:str,
-				mandatory: true,
-			}
-		},
-		scope: 'regular'
-	})
-}
+chrome.proxy.settings.set({
+	value: {
+		mode: "pac_script",
+		pacScript: {
+			data: String(function FindProxyForURL(url, host) {
+				var onoff = localStorage.getItem("poseidon_onoff");
+				console.log(onoff)
+				if(onoff){
+					var proxyList = localStorage.getItem("proxy_list");
+					var proxy;
+					for (var i = 0; i < proxyList.length; i++) {
+						if (proxyList[i].host==host&& ~url.indexOf(proxyList[i].perfix) ){
+							proxy=proxyList[i]
+						}
+					}
+					if (proxy==null){
+						return 'DIRECT'
+					}else {
+						return 'PROXY '+proxy.target+'; DIRECT'
+					}
+				}
+				return 'DIRECT'
+			}),
+			mandatory: true,
+		}
+	},
+	scope: 'regular'
+});
+chrome.webRequest.onBeforeSendHeaders.addListener(
+	function(details) {
+		var headers = details.requestHeaders;
+		headers.push({
+			name: 'x-tif-uid',
+			value: '755150713268621312'
+		});
+		return {requestHeaders: details.requestHeaders};
+	},
+	{urls: ["http://ipcrio-dev-121.pdcts.com.cn/!*"]},
+	["blocking", "requestHeaders"]
+);
 
