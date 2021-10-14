78efe057e511e478e108e8de9b9304932ca06498
muggle <isocket@outlook.com>
2021-10-14 17:55:31 +0800

    feat(1.0.0): 打版本号

--- background.js
+++ background.js
@@ -12,19 +12,4 @@ function setProxy (data){
 	});
 }
 
-function setHeader(headerObj){
-	chrome.webRequest.onBeforeSendHeaders.addListener(
-		function(details) {
-			var headers = details.requestHeaders;
-			headers.push({
-				name: 'x-tif-uid',
-				value: '755150713268621312'
-			});
-			return {requestHeaders: details.requestHeaders};
-		},
-		{urls: ["http://ipcrio-dev-121.pdcts.com.cn/!*"]},
-		["blocking", "requestHeaders"]
-	);
-}
-
 
